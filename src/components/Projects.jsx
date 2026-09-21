97f1b2efd9fcaae1fca3df7a176a6e1b1cf5a30b
Fathima Safiya <fathima.safiya.tech@gmail.com>
2026-09-21 16:07:23 +0530

    Fix typo in Projects.jsx import statement

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,4 @@
-1import { useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import hallcamillaImg from '../assets/Portfolio/hallcamilla.png';
